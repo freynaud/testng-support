f3b6e7a564af3337c7ace1e8666cdf775ada6341
Francois Reynaud <francois.reynaud@gmail.com>
2011-11-16 05:48:18 -0800

    cleanup

--- src/main/java/org/openqa/selenium/support/testng/WebTestSession.java
+++ src/main/java/org/openqa/selenium/support/testng/WebTestSession.java
@@ -6,8 +6,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.openqa.grid.common.SeleniumProtocol;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverBackedSelenium;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -47,7 +49,7 @@ public class WebTestSession {
       selenium.start();
       put(SELENIUM, selenium);
     } else {
-      WebDriver driver = new RemoteWebDriver(url, DesiredCapabilities.firefox());
+      WebDriver driver = new RemoteWebDriver(url, getCapability());
       put(WEBDRIVER, driver);
       WebDriverBackedSelenium selenium = new WebDriverBackedSelenium(driver, START_URL);
       put(SELENIUM, selenium);
@@ -56,6 +58,17 @@ public class WebTestSession {
   }
 
 
+  private static DesiredCapabilities getCapability(){
+    DesiredCapabilities cap = new DesiredCapabilities();
+    cap.setBrowserName((String)get(CapabilityType.BROWSER_NAME));
+    cap.setVersion((String)get(CapabilityType.VERSION));
+    String p = (String)get(CapabilityType.PLATFORM);
+    if (p!=null){
+      Platform pl = Platform.valueOf(p);
+      cap.setPlatform(pl);
+    }
+    return cap;
+  }
   
   
   public static void stop() {
